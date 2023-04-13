87d88147f079295aed89916b0d3a36429b80eb0e
Jungro <lee.jungro@gmail.com>
2023-04-13 18:07:47 -0500

    resolve type error for error set as unknown'

--- jl-personal/src/pages/api/sendEmail.ts
+++ jl-personal/src/pages/api/sendEmail.ts
@@ -8,18 +8,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { name, email, message } = req.body;
 
     const msg = {
-        to: process.env.RECIPIENT_EMAIL,
-        from: `${name} <lee.jungro@yopmail.com>`,
-        subject: 'New message from the contact form',
-        text: message,
+      to: process.env.RECIPIENT_EMAIL,
+      from: `${name} <lee.jungro@yopmail.com>`,
+      subject: 'New message from the contact form',
+      text: message,
     };
 
     try {
       await sgMail.send(msg);
       res.status(200).json({ message: 'Email sent successfully' });
-    } catch (error) {
+    } catch (error: unknown) {
       console.error('Error sending email:', error); // Keep this line
-      console.error('Error details:', error.response?.body); // Add this line
+      if (error instanceof Error) {
+        // You can access the 'message' property since the error is now of type Error
+        console.error('Error details:', error.message);
+      }
       res.status(500).json({ error: 'Failed to send email' });
     }
   } else {
